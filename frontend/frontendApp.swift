2ce6f58ca1b04b817bdddbc50a51125ffa86ab27
Anastasia <n.gusak04@gmail.com>
2024-10-11 20:12:11 +0300

    Initial Commit

---
+++ frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Анастасия Гусак on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
